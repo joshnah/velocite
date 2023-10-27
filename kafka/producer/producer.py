ff785d097c2507e781047452ed95c44dd003d053
Tien Dat Phan <tien-dat.phan@grenoble-inp.org>
2023-10-27 15:37:09 +0000

    Ordonannceur

--- kafka/producer/producer.py
+++ kafka/producer/producer.py
@@ -1,15 +1,29 @@
 import json
+import queue
+import threading
 import time
-from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaConsumer
 import requests
 
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TOPIC_NAME = os.getenv("TOPIC_NAME")
 SERVER_ADDRESS = os.getenv("SERVER_ADDRESS")
+RESULT_TOPIC = os.getenv("RESULT_TOPIC")
+TODO_CITIES_TOPIC = os.getenv("TODO_CITIES_TOPIC")
+
+list_apis = {"paris": "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records",
+           "lille": "https://opendata.lillemetropole.fr/api/explore/v2.1/catalog/datasets/vlille-realtime/records",
+           "lyon": "https://transport.data.gouv.fr/gbfs/lyon/station_information.json",
+           "strasbourg": "https://data.strasbourg.eu/api/explore/v2.1/catalog/datasets/stations-velhop/records",
+           "toulouse": "https://data.toulouse-metropole.fr/api/explore/v2.1/catalog/datasets/api-velo-toulouse-temps-reel/records",
+           "bordeaux": "https://transport.data.gouv.fr/gbfs/vcub/station_information.json",
+           "nancy": "https://transport.data.gouv.fr/gbfs/nancy/station_information.json",
+           "amiens": "https://transport.data.gouv.fr/gbfs/amiens/station_information.json",
+           "besancon": "https://transport.data.gouv.fr/gbfs/besancon/station_information.json"}
 
 
 producer = KafkaProducer(
@@ -17,11 +31,124 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
+consumer = KafkaConsumer(
+    TODO_CITIES_TOPIC,
+    bootstrap_servers=SERVER_ADDRESS,
+    group_id="producer"
+)
 
-def calling_api() -> dict:
-    # Calling API
-    answer = requests.get("https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=20").json()
-    return answer
+
+def extract_from_opendata_thread(city, thread_id, number_of_threads, merged_results):
+    offset = thread_id * 100
+    results = []
+    while True:
+        url = list_apis[city] + "?limit=100&offset=" + str(offset)
+        try:
+            response = requests.get(url).json()
+        except Exception as e:
+            print(e)
+            print("Error while calling API from opendata")
+            exit(1)
+        if (len(response["results"]) > 0):
+            results = results + response["results"]
+            offset += number_of_threads * 100
+        else:
+            break
+    match city:
+        case "lille":
+            for station in results:
+                station["station_id"] = station["libelle"]
+                station["name"] = station["nom"]
+                station["capacity"] = station["nbplacesdispo"]
+                station["lat"] = station["geo"]["lat"]
+                station["lon"] = station["geo"]["lon"]
+                station["num_bikes_available"] = station["nbvelosdispo"]
+                station["last_reported"] = datetime.strptime(
+                    station["datemiseajour"][:-6], "%Y-%m-%dT%H:%M:%S").timestamp()
+        case "strasbourg":
+            for station in results:
+                station["name"] = station["na"]
+                station["capacity"] = station["to"]
+                station["lat"] = station["la"]
+                station["lon"] = station["lg"]
+                station["num_bikes_available"] = station["av"]
+                station["station_id"] = station["id"]
+        case "toulouse":
+            for station in results:
+                station["station_id"] = station["number"]
+                station["capacity"] = station["bike_stands"]
+                station["lat"] = station["position"]["lat"]
+                station["lon"] = station["position"]["lon"]
+                station["num_bikes_available"] = station["available_bikes"]
+                station["last_reported"] = datetime.strptime(
+                    station["last_update"][:-6], "%Y-%m-%dT%H:%M:%S").timestamp()
+        case default:
+            for station in results:
+                station["lat"] = station["coordonnees_geo"]["lat"]
+                station["lon"] = station["coordonnees_geo"]["lon"]
+                station["num_bikes_available"] = station["numbikesavailable"]
+                station["station_id"] = station["stationcode"]
+                station["last_reported"] = datetime.strptime(
+                    station["duedate"][:-6], "%Y-%m-%dT%H:%M:%S").timestamp()
+    merged_results.put(results)
+
+
+def extract_from_opendata(city):
+    merged_results = queue.Queue()
+    total = 0
+    url = list_apis[city]
+    try:
+        response = requests.get(url).json()
+        total = response["total_count"]
+    except Exception as e:
+        print(e)
+        print("Error while calling API from opendata")
+        exit(1)
+    number_of_pages = total // 100 + 1
+    max_threads = 10
+    number_of_threads = min(max_threads, number_of_pages)
+    threads = []
+    for i in range(number_of_threads):
+        t = threading.Thread(target=extract_from_opendata_thread, args=(
+            city, i, number_of_threads, merged_results))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+
+    stations = [station for sublist in list(
+        merged_results.queue) for station in sublist]
+    return {'stations': stations, 'city': city}
+
+
+def extract_from_gouv(city):
+    try:
+        stations_informations = requests.get(list_apis[city]).json()
+    except Exception as e:
+        print(e)
+        print("Error while calling API informations from gouv")
+        exit(1)
+    stations_informations = stations_informations["data"]["stations"]
+    try:
+        stations_status = requests.get(list_apis[city].replace(
+            "station_information", "station_status")).json()
+    except Exception as e:
+        print(e)
+        print("Error while calling API status from gouv")
+        exit(1)
+    stations_status = stations_status["data"]["stations"]
+    stations_dict = {}
+    for station in stations_informations:
+        stations_dict[station["station_id"]] = station
+    for station in stations_status:
+        informations = stations_dict[station["station_id"]]
+        station["name"] = informations["name"]
+        station["lat"] = informations["lat"]
+        station["lon"] = informations["lon"]
+        station["capacity"] = informations["capacity"]
+
+    return {'stations': stations_status, 'city': city}
+
 
 if __name__ == "__main__":
     """
@@ -31,10 +158,14 @@ if __name__ == "__main__":
     """
 
     try:
-        while(True):
-            result = calling_api()
-            print(result)
-            producer.send(TOPIC_NAME, result)
-            time.sleep(10)
+        for city, url in list_apis.items():
+            if (url.find(".json") == -1):
+                print("extract from " + city + " opendata")
+                result = extract_from_opendata(city)
+            else:
+                print("extract from " + city + " gouv")
+                result = extract_from_gouv(city)
+            producer.send(RESULT_TOPIC, result)
+        producer.flush()
     except KeyboardInterrupt:
-       print("Quit")
+        print("Quit")
