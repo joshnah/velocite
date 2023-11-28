a915c7a2a68f9d335b3e2ba0a75878d114305739
Antoine Klein <antoineklein2000@gmail.com>
2023-11-28 12:14:14 +0100

    liste des villes / liste stations offset todo

--- webserver/app/main.py
+++ webserver/app/main.py
@@ -26,14 +26,25 @@ app = FastAPI()
 
 @app.get("/")
 def read_root(request : Request):
-    row = session.execute(
+    rows = session.execute(
         "SELECT DISTINCT city FROM station.stations")
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content={"cities": [{r.city: str(request.url) + r.city} for r in row]})
+        content={"cities": [{r.city: str(request.base_url) + r.city} for r in rows]})
 
 
-# Get Cassandra cluster name and listen address
+# Get list of stations in a city
 @app.get("/{city}/")
-def read_city(city, page,request):
-    return city, page, request.url
+def read_city(city,  request : Request, page = 1):
+    max_station_page = 100
+    print(request.base_url)
+    page = int(page)
+    if page < 1:
+        page = 1
+    rows = session.execute(
+        "SELECT station_id FROM station.stations WHERE city = '%s' LIMIT %s;" % (city, max_station_page)) # , (page - 1) * max_station_page
+    rows = list(rows)
+    more_station = len(rows) >= max_station_page
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"stations": [{r.station_id: str(request.base_url)+ '/' + city + '/' + r.station_id} for r in rows], "next_page": str(request.base_url) + '/' + city + '/' + "?page=" + str(page + 1) if more_station else None})
