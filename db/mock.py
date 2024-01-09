6148695660c70569d0a24f50076c5b247952d9ab
Antoine Klein <antoineklein2000@gmail.com>
2024-01-09 10:17:38 +0100

    not functionnal

---
+++ db/mock.py
@@ -0,0 +1,76 @@
+from cassandra.cluster import Cluster
+from datetime import datetime, timedelta
+import requests
+import random
+
+
+# get the list of station from the api
+url = "https://transport.data.gouv.fr/gbfs/lyon/station_information.json"
+
+
+def get_station_list(url):
+    response = requests.get(url)
+    return [station["station_id"] for station in  response.json()["data"]["stations"]]
+
+def create_session():
+    cluster = Cluster(['localhost'])
+    session = cluster.connect('station')  # Replace 'your_keyspace' with your actual keyspace
+    return session
+
+def insert_data(session, data):
+    query = """
+        INSERT INTO stations (city, station_id, updated_at, bikes, capacity)
+        VALUES (%(city)s, %(station_id)s, %(updated_at)s, %(bikes)s, %(capacity)s)
+    """
+    session.execute(query, data)
+
+def insert_weather(session, current_date):
+    proba_rain = random.randint(0, 100)
+    query_select = """
+        SELECT DISTINCT city, station_id FROM station.stations;
+    """
+    result_set = session.execute(query_select)
+    query = """
+        INSERT INTO stations (city, station_id, updated_at, proba_rain) 
+        VALUES (%(city)s, %(station_id)s, %(updated_at)s, %(proba_rain)s)
+    """
+    # pour chaque couple city, station_id on insère une proba de pluie
+    for row in result_set:
+        mock_data = {
+            "city": row.city,
+            "station_id": row.station_id,
+            "updated_at": current_date,
+            "proba_rain": proba_rain
+        }
+        session.execute(query, mock_data)
+    
+    
+
+def insert_mock_data(session, station_ids, start_date, end_date):
+    current_date = start_date
+    while current_date <= end_date:
+        for station_id in station_ids:
+            random_bikes = random.randint(0, 20)
+            mock_data = {
+                "city": "lyon",
+                "station_id": station_id,
+                "updated_at": current_date,
+                "bikes": random_bikes,
+                "capacity": 20  # You can adjust these values as needed
+            }
+            insert_data(session, mock_data)
+        current_date += timedelta(hours=1)
+    while current_date <= end_date + timedelta(hours=12):
+        insert_weather(session, current_date)
+        current_date += timedelta(hours=1)
+        
+
+if __name__ == "__main__":
+    session = create_session()
+    # flush the table
+    session.execute("TRUNCATE stations")
+    start_date = datetime(2023, 12, 2, 0, 0, 0)
+    end_date = datetime.now()
+
+    station_ids = get_station_list(url)
+    insert_mock_data(session, station_ids, start_date, end_date)
--- spark/prediction.py
+++ spark/prediction.py
@@ -1,8 +1,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
 from pyspark.sql import functions as F
-from pyspark.sql.functions import from_json, col, explode, from_unixtime,window,avg,date_format, expr
+from pyspark.sql.functions import from_json, col, explode, from_unixtime,window,avg,date_format, expr, broadcast
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, ArrayType, TimestampType
+
 import sys
 # Define the schema for the Kafka message
 schema = StructType([
@@ -43,62 +44,74 @@ def main():
 
     spark.sparkContext.setLogLevel("ERROR")
 
-    # select * from station.weather and print result
-    # spark.read \
-    #     .format("org.apache.spark.sql.cassandra") \
-    #     .options(table="weather", keyspace="station") \
-    #     .load() \
-    #     .show()
-    
-    # join station.weather and station.stations and print result (station.weather.city = station.stations.city and station.weather.forecast_date + INTERVAL 30 MINUTES <= station.stations.updated_at AND station.weather.forecast_date - INTERVAL 30 MINUTES >= station.stations.updated_at)
-    
-    # df_weather = spark.read.format("org.apache.spark.sql.cassandra").options(table="weather", keyspace="station").load()
-    # df_stations = spark.read.format("org.apache.spark.sql.cassandra").options(table="stations", keyspace="station").load()
-
-    # result = df_weather.join(df_stations.withColumnRenamed("updated_at", "stations_updated_at"), ["city"], "inner") \
-    #     .where(expr("forecast_date + INTERVAL 30 MINUTES <= stations_updated_at") &
-    #         expr("forecast_date - INTERVAL 30 MINUTES >= stations_updated_at"))
-
     df_stations = spark.read.format("org.apache.spark.sql.cassandra").options(table="stations", keyspace="station").load()
+    # Trier le DataFrame par city, station_id et updated_at
+    sorted_stations = df_stations.orderBy("city", "station_id", "updated_at")
 
-    # Define a window specification based on the "updated_at" column
+    # Définir la fenêtre de partition
     window_spec = Window.partitionBy("city", "station_id").orderBy("updated_at")
 
-    # Calculate the difference between the current bikes and the previous bikes
-    df_stations_diff = df_stations.withColumn("bikes_diff", F.col("bikes") - F.lag("bikes").over(window_spec))
+    # Calculer la différence de vélos entre chaque heure
+    diff_bikes = sorted_stations.withColumn("diff_bikes", F.col("bikes") - F.lag("bikes").over(window_spec))
 
+    # Calculer la moyenne pondérée par la probabilité de pluie
+    avg_diff_bikes = diff_bikes.groupBy("city", "station_id", F.hour("updated_at").alias("hour")) \
+        .agg(F.avg(F.when(F.col("proba_rain").isNotNull(), F.col("diff_bikes") * F.col("proba_rain")).otherwise(F.col("diff_bikes"))).alias("avg_diff_bikes"))
+    avg_diff_bikes.show()
+    # Joindre les données de probabilité future de pluie
+    final_data = avg_diff_bikes.join(
+    df_stations.select("city", "station_id", F.hour("updated_at").alias("hour"), "proba_rain", "bikes", "capacity"),
+    on=["city", "station_id", "hour"],
+    how="left_outer"
+    )
 
-    df_weather = spark.read.format("org.apache.spark.sql.cassandra").options(table="weather", keyspace="station").load()
-    result = df_weather.join(df_stations_diff.withColumnRenamed("updated_at", "stations_updated_at"), ["city"], "inner") \
-    .where(expr("forecast_date + INTERVAL 30 MINUTES <= stations_updated_at") &
-        expr("forecast_date - INTERVAL 30 MINUTES >= stations_updated_at"))
+    # Utiliser la colonne avg_diff_bikes pour le calcul de la prédiction
+    final_data = final_data.withColumn("prediction",
+        F.when(F.col("proba_rain").isNotNull(), F.col("bikes") + F.col("avg_diff_bikes") * F.col("proba_rain")).otherwise(F.col("bikes"))
+    )
 
-    result = result.withColumn("hour_of_forecast", F.hour("forecast_date"))
+    # Respecter les contraintes de capacité
+    capped_prediction = final_data.selectExpr(
+        "*",
+        "LEAST(GREATEST(prediction, 0), capacity) as capped_prediction"
+    )
+    # Afficher le résultat
+    capped_prediction.show()
+#     df_stations_null = df_stations.filter(df_stations.bikes.isNull())
+#     # filter rows where bikes is null
+#     df_stations = df_stations.filter(df_stations.bikes.isNotNull())
 
-    result_grouped = result.groupBy("station_id", "city", "hour_of_forecast").agg(
-    F.first("bikes").alias("initial_bikes"),
-    (F.avg(F.expr("bikes_diff * 100 / (100 - proba_rain)"))).alias("weighted_avg_bikes_diff")
-)
+#     # Define a window specification based on the "updated_at" column
+#     window_spec = Window.partitionBy("city", "station_id").orderBy("updated_at")
 
-    df_weather_forecast = df_weather\
-        .withColumn("hour_of_forecast", F.hour("forecast_date")) \
-        .where(expr("forecast_date >= current_timestamp()")) \
-    
-    df_final = df_weather_forecast.join(result_grouped, ["hour_of_forecast", "city"], "inner") 
-    df_final = df_final.withColumn("prediction", F.col("initial_bikes") + F.coalesce(F.col("weighted_avg_bikes_diff"), F.lit(0)))
+#     # On crée une colonne dans la table stations pour avoir la différence entre chaque heure
+#     df_stations_diff = df_stations.withColumn("bikes_diff", F.col("bikes") - F.lag("bikes").over(window_spec))
+#     # replace null value by 0
+#     df_stations_diff = df_stations_diff.na.fill(0)
+#     df_hour = df_stations_diff.withColumn("hour_of_forecast", F.hour("updated_at"))
+#     # order by updated_at desc
+#     df_hour = df_hour.orderBy("updated_at", ascending=False)
+#     result_grouped = df_hour.groupBy("station_id", "city", "hour_of_forecast").agg(
+#     F.first("bikes").alias("initial_bikes"),
+#     (F.avg(F.expr("bikes_diff * 100 / (100 - proba_rain)"))).alias("weighted_avg_bikes_diff")
+# )
+#     # join result_grouped with df_stations to get the capacity
+#     df_final = result_grouped.join(df_stations, ["station_id", "city"])
+#     # show the result
+#     df_final = df_final.withColumn("prediction", F.col("initial_bikes") + F.coalesce(F.col("weighted_avg_bikes_diff"), F.lit(0)))
 
-    window_forecast = Window.partitionBy("city", "station_id").orderBy("hour_of_forecast")
+#     window_forecast = Window.partitionBy("city", "station_id").orderBy("hour_of_forecast")
 
-    df_final_diff = df_final.withColumn(
-    "prediction",
-    F.when(F.col("weighted_avg_bikes_diff") + F.lag("prediction").over(window_forecast) < 0, 0)
-    .otherwise(F.col("weighted_avg_bikes_diff") + F.lag("prediction").over(window_forecast))
-).withColumnRenamed("updated_at", "up_at").withColumnRenamed("forecast_date", "updated_at")
-    df_final_diff.select("station_id", "city", "updated_at", "prediction").write \
-    .format("org.apache.spark.sql.cassandra") \
-    .options(table="stations", keyspace="station") \
-    .mode("append") \
-    .save()
+#     df_final_diff = df_final.withColumn(
+#     "prediction",
+#     F.when(F.col("weighted_avg_bikes_diff") + F.lag("prediction").over(window_forecast) < 0, 0)
+#     .otherwise(F.col("weighted_avg_bikes_diff") + F.lag("prediction").over(window_forecast))
+# ).withColumnRenamed("updated_at", "up_at").withColumnRenamed("forecast_date", "updated_at")
+#     df_final_diff.select("station_id", "city", "updated_at", "prediction").write \
+#     .format("org.apache.spark.sql.cassandra") \
+#     .options(table="stations", keyspace="station") \
+#     .mode("append") \
+#     .save()
 
 
 if __name__ == "__main__":
