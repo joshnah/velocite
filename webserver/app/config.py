f18501a94e0df5ce17028b26ba06e5e8c73a80df
Viviane Qian <viviane.qian@grenoble-inp.org>
2024-01-22 15:06:01 +0100

    Remove webserver

--- webserver/app/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class Settings(BaseSettings):
-    cassandra_host: str
-    cassandra_port: int
-
-    model_config = SettingsConfigDict(env_file=".env")
